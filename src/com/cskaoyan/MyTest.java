3362e17582f558bed396947f4aa550c60f78ec5a
feiyu-za <13880665270@163.com>
2019-04-02 20:18:18 +0800

    这是通过IDEA提交的

--- src/com/cskaoyan/MyTest.java
+++ src/com/cskaoyan/MyTest.java
@@ -5,7 +5,7 @@ public class MyTest {
         System.out.println("hello idea git");
         System.out.println("hello idea git");
         new MyTest().newMethod();
-        System.out.println("this is repo3 by 0402");
+        System.out.println("0402晚");
         System.out.println("this is update by idea_repo");
 
     }
